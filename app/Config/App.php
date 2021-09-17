2bc3cd20d048bb86608dcdf045147c92046d824a
Ronald Barry Bonje <ronald.bonje@mvp.dev>
2021-09-17 18:56:39 +0800

    .

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://xtrabits.herokuapp.com/';
+    public $baseURL = 'https://xtrabits.herokuapp.com/:8080/';
 
     /**
      * --------------------------------------------------------------------------
@@ -36,7 +36,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
